12fee7b7beb1ac9489df8e46d6866a48a6da161f
Tron <achuan-2@outlook.com>
2023-10-18 22:26:31 +0800

    :sparkels: support save and load config info

--- +utils/save_file.m
+++ +utils/save_file.m
@@ -1,6 +1,47 @@
-function [filename,path]= save_file(fileExtension)
-% fileExtension: {'*.jpg';'*.png';'tif'}
-	f_dummy = figure('Position', [-100 -100 0 0]); %create a dummy figure so that uigetfile doesn't minimize our GUI
-	[filename, path] = uiputfile(fileExtension, 'Save as');
-	delete(f_dummy); %delete the dummy figure
+function [filename,selectedDir]= save_file(fileExtension,matFile)
+%save_file(  - 打开弹窗，保存文件
+%
+%   USAGE
+%        [filename,selectedDir]= save_file()
+%        [filename,selectedDir]= save_file({'.m'},'lastUsedImagePath.mat')
+%
+%   Input Arguments
+%        fileExtension     -   默认保存文件的类型，默认为All Files (*.*)
+%        matFile   -   保存上次选择路径的mat文件名
+%
+%   Output Arguments
+%        filename          -   要保存文件的文件名
+%        selectedDir       -   要保存文件的所在路径
+%
+
+
+        %create a dummy figure so that uigetfile doesn't minimize our GUI
+        f_dummy = figure('Position', [-100 -100 0 0]); 
+        
+        if  nargin < 1
+            fileExtension = {'*.*',  'All Files (*.*)'};
+        end
+        if  nargin < 2
+            matFile = 'lastUsedDir.mat';
+        end
+
+        filePath = mfilename('fullpath'); % get .m  script path
+        [currentPath, ~, ~] = fileparts(filePath);
+        lastUsedDirFile = fullfile(currentPath,matFile);
+        if exist(lastUsedDirFile, 'file')
+            load(lastUsedDirFile, 'lastUsedDir');
+            [filename,selectedDir] = uiputfile(fileExtension, 'Save as',lastUsedDir);
+        else
+            [filename,selectedDir] = uiputfile(fileExtension, 'Save as');
+        end
+
+        % if a dir is selected,save the path to lastUsedDir.mat
+        if filename ~= 0
+            lastUsedDir = selectedDir;
+            save(lastUsedDirFile, 'lastUsedDir');
+        end
+
+        %delete the dummy figure
+        delete(f_dummy); 
+
 end
--- +utils/select_dir.m
+++ +utils/select_dir.m
@@ -1,13 +1,25 @@
-function path= select_dir(defaultPath)
-
-        f_dummy = figure('Position', [-100 -100 0 0]); %create a dummy figure so that uigetfile doesn't minimize our GUI
-        if nargin < 1
-            path = uigetdir();
-        else
-            path = uigetdir(defaultPath);
+function selectedDir = select_dir(default_path)
+%select_dir - 打开弹窗，选择文件夹
+%
+%   USAGE
+%        selectedDir = select_dir()
+%        selectedDir = select_dir(default_path)
+%
+%   Input Arguments
+%        matFile   -   保存上次选择路径的mat文件名
+%
+%   Output Arguments
+%        selectedDir       -   所选文件的所在路径
+        arguments
+            default_path = ''
         end
+        % create a dummy figure so that uigetfile doesn't minimize our GUI
+        f_dummy = figure('Position', [-100 -100 0 0]);
+        
+
+        selectedDir = uigetdir(default_path);
+
+        
+
         delete(f_dummy); %delete the dummy figure
-
 end
-
-
